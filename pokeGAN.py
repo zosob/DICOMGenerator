0364211049901f371212ecb3cade5956bd2007e8
Bhaskar Ghosh <bhaskar4647@gmail.com>
2021-03-15 13:25:02 -0500

    Added Discriminator and Train function

--- pokeGAN.py
+++ pokeGAN.py
@@ -80,3 +80,154 @@ def generator(input, random_dim, is_train, reuse = False):
         #bn6 = tf.contrib.layers.batch_norm(conv6, is_training=is_train, epsilon=1e-5, decay=0.9, updates_collections=None, scope='bn6')
         act6 = tf.nn.tanh(conv6, name='act6')
         return act6
+
+def discriminator(input, is_train, reuse=False):
+    c2, c4, c8, c16 = 64, 128, 256, 512 #Channel numbers
+    with tf.variable_scope('dis') as scope:
+        if reuse:
+            scope.reuse_variables()
+
+        #Convolution, activation, bias, repeat!
+        conv1 = tf.layers.conv2d(input, c2, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+                                 kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                                 name='conv1')
+        bn1 = tf.contrib.layers.batch_norm(conv1, is_training = is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope = 'bn1')
+        act1 = lrelu(conv1, n='act1')
+        #Convolution, activation, bias, repeat!
+        conv2 = tf.layers.conv2d(act1, c4, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+                                 kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                                 name='conv2')
+        bn2 = tf.contrib.layers.batch_norm(conv2, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn2')
+        act2 = lrelu(bn2, n='act2')
+        #Convolution, activation, bias, repeat!
+        conv3 = tf.layers.conv2d(act2, c8, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+                                 kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                                 name='conv3')
+        bn3 = tf.contrib.layers.batch_norm(conv3, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn3')
+        act3 = lrelu(bn3, n='act3')
+         #Convolution, activation, bias, repeat!
+        conv4 = tf.layers.conv2d(act3, c16, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
+                                 kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                                 name='conv4')
+        bn4 = tf.contrib.layers.batch_norm(conv4, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn4')
+        act4 = lrelu(bn4, n='act4')
+        # wgan just get rid of the sigmoid
+        logits = tf.add(tf.matmul(fc1, w2), b2, name='logits')
+        # dcgan
+        acted_out = tf.nn.sigmoid(logits)
+        return logits #, acted_out
+
+def train():
+    random_dim = 100
+
+    with tf.variable_scope('input'):
+        #real and fake image placholders
+        real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
+        random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
+        is_train = tf.placeholder(tf.bool, name='is_train')
+
+    # wgan
+    fake_image = generator(random_input, random_dim, is_train)
+
+    real_result = discriminator(real_image, is_train)
+    fake_result = discriminator(fake_image, is_train, reuse=True)
+
+    d_loss = tf.reduce_mean(fake_result) - tf.reduce_mean(real_result)  # This optimizes the discriminator.
+    g_loss = -tf.reduce_mean(fake_result)  # This optimizes the generator.
+
+
+    t_vars = tf.trainable_variables()
+    d_vars = [var for var in t_vars if 'dis' in var.name]
+    g_vars = [var for var in t_vars if 'gen' in var.name]
+    trainer_d = tf.train.RMSPropOptimizer(learning_rate=2e-4).minimize(d_loss, var_list=d_vars)
+    trainer_g = tf.train.RMSPropOptimizer(learning_rate=2e-4).minimize(g_loss, var_list=g_vars)
+    # clip discriminator weights
+    d_clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in d_vars]
+
+
+    batch_size = BATCH_SIZE
+    image_batch, samples_num = process_data()
+
+    batch_num = int(samples_num / batch_size)
+    total_batch = 0
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+    # continue training
+    save_path = saver.save(sess, "/tmp/model.ckpt")
+    ckpt = tf.train.latest_checkpoint('./model/' + version)
+    saver.restore(sess, save_path)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    print('total training sample num:%d' % samples_num)
+    print('batch size: %d, batch num per epoch: %d, epoch num: %d' % (batch_size, batch_num, EPOCH))
+    print('start training...')
+    for i in range(EPOCH):
+        print("Running epoch {}/{}...".format(i, EPOCH))
+        for j in range(batch_num):
+            print(j)
+            d_iters = 5
+            g_iters = 1
+
+            train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
+            for k in range(d_iters):
+                print(k)
+                train_image = sess.run(image_batch)
+                #wgan clip weights
+                sess.run(d_clip)
+
+                # Update the discriminator
+                _, dLoss = sess.run([trainer_d, d_loss],
+                                    feed_dict={random_input: train_noise, real_image: train_image, is_train: True})
+
+            # Update the generator
+            for k in range(g_iters):
+                # train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
+                _, gLoss = sess.run([trainer_g, g_loss],
+                                    feed_dict={random_input: train_noise, is_train: True})
+
+            # print 'train:[%d/%d],d_loss:%f,g_loss:%f' % (i, j, dLoss, gLoss)
+
+        # save check point every 500 epoch
+        if i%500 == 0:
+            if not os.path.exists('./model/' + version):
+                os.makedirs('./model/' + version)
+            saver.save(sess, './model/' +version + '/' + str(i))
+        if i%50 == 0:
+            # save images
+            if not os.path.exists(newPoke_path):
+                os.makedirs(newPoke_path)
+            sample_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
+            imgtest = sess.run(fake_image, feed_dict={random_input: sample_noise, is_train: False})
+            # imgtest = imgtest * 255.0
+            # imgtest.astype(np.uint8)
+            save_images(imgtest, [8,8] ,newPoke_path + '/epoch' + str(i) + '.jpg')
+
+            print('train:[%d],d_loss:%f,g_loss:%f' % (i, dLoss, gLoss))
+    coord.request_stop()
+    coord.join(threads)
+    
+# def test():
+    # random_dim = 100
+    # with tf.variable_scope('input'):
+        # real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
+        # random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
+        # is_train = tf.placeholder(tf.bool, name='is_train')
+
+    # # wgan
+    # fake_image = generator(random_input, random_dim, is_train)
+    # real_result = discriminator(real_image, is_train)
+    # fake_result = discriminator(fake_image, is_train, reuse=True)
+    # sess = tf.InteractiveSession()
+    # sess.run(tf.global_variables_initializer())
+    # variables_to_restore = slim.get_variables_to_restore(include=['gen'])
+    # print(variables_to_restore)
+    # saver = tf.train.Saver(variables_to_restore)
+    # ckpt = tf.train.latest_checkpoint('./model/' + version)
+    # saver.restore(sess, ckpt)
+
+if __name__ == "__main__":
+    train()
+    #test()
